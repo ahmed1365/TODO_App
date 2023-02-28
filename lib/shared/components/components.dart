c88eafdcbdd8d6d880a1554b80fe6efc0ac1eb56
Ahmed Mahran Mohamed <67957117+ahmed1365@users.noreply.github.com>
2023-02-28 15:03:56 +0200

    Update components.dart

--- lib/shared/components/components.dart
+++ lib/shared/components/components.dart
@@ -2,34 +2,7 @@ import 'package:ahmed/shared/cubit/cubit.dart';
 import 'package:conditional_builder/conditional_builder.dart';
 import 'package:flutter/material.dart';
 
-Widget defaultButtons({
-    double width = double.infinity ,
-    Color background = Colors.teal,
-    bool  isUpperCase = true,
-    double radius = 3.0,
-   required Function function,
-   required String text,
-}) =>
-    Container(
-  width: width,
-      height: 50.0,
-      child: MaterialButton(
-    onPressed: (){
-      function();
-    } ,
-    child: Text(
-    isUpperCase ?   text.toUpperCase() : text,
-      style: TextStyle(
-        color: Colors.white,
-      ),
-    ),
 
-  ),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(radius),
-        color: background,
-      ),
-);
 
 Widget buildTaskItem(Map model, context) => Dismissible(
   key: Key(model['id'].toString()),
